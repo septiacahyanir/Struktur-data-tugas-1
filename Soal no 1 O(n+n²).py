0c50d367b845dcfa174d6b0c74d600d83310bb05
septiacahyanir <septiacahyani81@gmail.com>
2025-03-03 21:35:50 +0700

    Update Soal no 1 O(n+n²).py

--- Soal no 1 O(n+n²).py
+++ Soal no 1 O(n+n²).py
@@ -3,8 +3,8 @@ def print_items(n):
     for i in range(n):
         print(f"Item O(n): {i}")  
 
-    # Bagian O(n^2)
-    for i in range(n):
+    # Bagian O(n)
+    for i in range(n²):
         for j in range(n):
             print(f"Item O(n²): {i}, {j}")  
             
